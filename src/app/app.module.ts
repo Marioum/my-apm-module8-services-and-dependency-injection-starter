7f722cd0e1a50b2e6f63c4a03e281508902be718
Mariem Amari <ammeri.mariem.info@gmail.com>
2018-12-12 09:47:10 +0100

    Services an Dependency Injection

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsFilterPipe } from './pipes/products-filter.pipe';
+import { StarComponent } from './components/shared/star/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent,
+    ProductsFilterPipe,
+    StarComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../../domain/iproduct';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+export class ProductsListComponent implements OnInit {
+
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = '';
+
+  products: IProduct[];
+
+  constructor() { } // Dependency injection !
+
+  ngOnInit() {
+
+     console.log('In OnInit');
+  }
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+  onRatingClicked(event): void {
+        this.pageTitle = 'Product List: ' + event;
+    }
+
+}
