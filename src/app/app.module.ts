be866f20030ec2ff4187bc82bb943a6aae85906b
Mariem Amari <ammeri.mariem.info@gmail.com>
2018-12-12 15:13:03 +0100

    router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,36 @@ import { AppComponent } from './app.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductsFilterPipe } from './pipes/products-filter.pipe';
 import { StarComponent } from './components/shared/star/star.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule, Route } from '@angular/router';
+
+const routes: Route[] = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'list', component: ProductsListComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
     ProductsFilterPipe,
-    StarComponent
+    StarComponent,
+    WelcomeComponent,
+    NotfoundComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
